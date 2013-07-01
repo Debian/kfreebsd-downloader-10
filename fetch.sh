8bd7225da35bbbbedb2b6ef32c79ace99cf9e9b8
Robert Millan <rmh@debian.org>
2013-07-01 22:07:02 +0000

    Remove version redundancy and extract everything from changelog.
    git-svn-id: svn://anonscm.debian.org/glibc-bsd/trunk/kfreebsd-downloader@4659 ae44cbe4-c7d5-0310-ae45-95c72a56cd7d

--- fetch.sh
+++ fetch.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-
-version="9.0"
-
-for arch in i386 amd64 ; do
-	wget http://ftp.freebsd.org/pub/FreeBSD/releases/$arch/$arch/${version}-RELEASE/MANIFEST -O MANIFEST.$arch
-done
+exec make -f debian/rules fetch
